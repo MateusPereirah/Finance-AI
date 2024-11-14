e94c1df50d24b5bb5c49f72bdaf6240f073fefd0
MateusPereirah <mateus.pereira1@outlook.com.br>
2024-11-14 11:53:25 -0300

    feat: finished authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-const Home = () => {
-  return <Button>Hello World</Button>;
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName></UserButton>
+    </div>
+  );
 };
 
 export default Home;
